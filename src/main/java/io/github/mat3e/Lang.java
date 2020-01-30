59e99aa5ed472022a5fb60ce6c137fd249c2fae1
efipe <poterala.f@gmail.com>
2020-01-30 13:32:36 +0100

    Added and configured hibernate DB migrated used Flyway

--- src/main/java/io/github/mat3e/Lang.java
+++ src/main/java/io/github/mat3e/Lang.java
@@ -1,22 +1,42 @@
 package io.github.mat3e;
 
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "languages")
 class Lang {
-    private Long id;
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Integer id;
     private String welcomeMsg;
     private String code;
 
-    public Long getId() {
+    /**
+     * hibernate ( jpa ) needs it
+     */
+    @SuppressWarnings("unused")
+    Lang() {
+    }
+
+    public Lang(Integer id, String welcomeMsg, String code) {
+        this.id = id;
+        this.welcomeMsg = welcomeMsg;
+        this.code = code;
+    }
+
+    public Integer getId() {
         return id;
     }
 
     public String getWelcomeMsg() {
         return welcomeMsg;
-    }
-
-    public Lang(Long id, String welcomeMsg, String code) {
-        this.id = id;
-        this.welcomeMsg = welcomeMsg;
-        this.code = code;
     }
 
     public void setWelcomeMsg(String welcomeMsg) {
